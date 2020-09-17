2a20aabb85f7860f75ecbdf3a8b36cd2cd17f318
Nikita <Balaganskij.NN@phystech.edu>
2020-09-17 23:37:40 +0300

    fix memory consumption on gpu

--- src/utils/storage.py
+++ src/utils/storage.py
@@ -8,18 +8,21 @@ class Storage:
         self.storage_size = storage_size
         self.p = p
 
-    def get(self, images):
+    def get(self, images: torch.Tensor):
         current_batch = []
+        device = images.device
         for image in images:
             image = image.unsqueeze(0)
             if len(self._data) < self.storage_size:
                 self._data.append(image)
-                current_batch.append(image)
+                current_batch.append(image.cpu())
             else:
                 if np.random.uniform() < self.p:
                     idx_to_replace = np.random.randint(self.storage_size)
-                    current_batch.append(self._data[idx_to_replace].clone())
-                    self._data[idx_to_replace] = image
+                    current_batch.append(
+                        self._data[idx_to_replace].clone().to(device)
+                    )
+                    self._data[idx_to_replace] = image.cpu()
                 else:
                     current_batch.append(image)
         return torch.cat(current_batch, dim=0)
