732e0f846ee032b79fd7b5546a2acca1d79c8a11
Nikita <Balaganskij.NN@phystech.edu>
2020-09-20 22:23:46 +0300

    add initialization utils

--- src/utils/init_student.py
+++ src/utils/init_student.py
@@ -12,7 +12,11 @@ def preprocess_sd(sd):
 def initialize_pretrained(path: str, model: Dict[str, torch.nn.Module]):
     state_dict = torch.load(path)
     for model_key in state_dict["model_state_dict"].keys():
-        preprocessed_sd = preprocess_sd(
-            state_dict["model_state_dict"][model_key]
-        )
-        model[model_key].load_state_dict(preprocessed_sd)
+        preprocessed_sd = state_dict["model_state_dict"][model_key]
+        try:
+            model[model_key].load_state_dict(preprocessed_sd)
+        except:
+            preprocessed_sd = preprocess_sd(
+                state_dict["model_state_dict"][model_key]
+            )
+            model[model_key].load_state_dict(preprocessed_sd)
