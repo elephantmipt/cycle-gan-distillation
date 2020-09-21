b03b92ee3bbd779976f70b831d8ec8fbb4c296a0
Nikita <Balaganskij.NN@phystech.edu>
2020-09-22 00:30:51 +0300

    fix

--- src/experiments/train/__init__.py
+++ src/experiments/train/__init__.py
@@ -16,6 +16,7 @@ from src.modules.discriminator import PixelDiscriminator, NLayerDiscriminator
 from src.callbacks.visualization import LogImageCallback
 from src.runner import CycleGANRunner as Runner
 from src.modules.loss import LSGanLoss
+from src.experiments.train.train_experiment import Experiment
 
 registry.Model(Generator)
 registry.Model(PixelDiscriminator)
