99f0d2409927936fb5de47b7f4b89424e0d0e5d5
Nikita <Balaganskij.NN@phystech.edu>
2020-09-22 21:45:46 +0300

    fix

--- src/utils/metrics.py
+++ src/utils/metrics.py
@@ -13,7 +13,7 @@ class VGG(nn.Module):
     def forward(self, x):
         slices = [1, 6, 11, 20, 29]
         features = []
-        for idx, layer in enumerate(self.layers.chidren()):
+        for idx, layer in enumerate(self.layers.children()):
             x = layer(x)
             if idx in slices:
                 features.append(x)
